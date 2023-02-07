af79c3b39abe1a315dffdc8ad6d436db67711bce
fredtoh <fredtoh@gmail.com>
2023-02-07 04:11:32 -0800

    Added test cases. Restore api views.

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -5,12 +5,13 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('api-token-auth', obtain_auth_token),
     path('', views.index, name='index'),
-    # path('menu', views.MenuItemView.as_view(), name='menu'),
-    # path('menu/<int:pk>', views.SingleMenuItemView.as_view(), name='menuitem'),
-    # path('book/', views.BookingViewSet.as_view({
-    #     'post':'create',
-    #     'get': 'list',
-    #     }), name="book"),
+    path('api-menu', views.MenuItemView.as_view()),
+    path('api-menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('api-book', views.BookingViewSet.as_view({
+        'post':'create',
+        'get': 'list',
+        })
+        ),
     path('about', views.about, name="about"),
     path('menu', views.menu, name='menu'),
     path('menu/<int:pk>', views.display_menu_items, name='menu_item'),
--- littlelemon/tests/test_views.py
+++ littlelemon/tests/test_views.py
@@ -1,9 +1,10 @@
 import json
-from django.test import Client, TestCase
+from django.test import TestCase
 from restaurant.models import Menu
 from restaurant.serializers import MenuItemSerializer
+from django.contrib.auth.models import User
 
-#TestCase class
+# TestCase class
 class MenuViewTest(TestCase):
     def setUp(self):
         Menu.objects.create(title="IceCream", price=20, inventory=10)
@@ -16,10 +17,47 @@ class MenuViewTest(TestCase):
         #     {"id":3,"title":"Chocolate Cake","price":"30.00","inventory":15},
         #     {"id":4,"title":"Lemon Pie","price":"40.00","inventory":8}
         #     ]
-        c = Client()
-        response = c.get('/restaurant/menu') # MenuItemView.as_view()
+        response = self.client.get('/restaurant/api-menu') # MenuItemView.as_view()
         response_obj = json.loads(response.content)
         items = Menu.objects.all()
         serialized_obj = MenuItemSerializer(items, many=True).data
         self.assertEqual(response_obj, serialized_obj)
-        
+
+class BookViewTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='test')
+        self.formdata = u'''{
+            "name": "Minnie Mouse",
+            "no_of_guests": 2,
+            "bookingDate": "2023-02-07T13:00"}'''
+
+    def test_non_authorization(self):
+        # No login and authentication
+        response = self.client.get('/restaurant/bookings') # views.reservations
+        self.assertEqual(response.status_code, 401)
+
+    def test_401(self):
+        # No login and authentication
+        json_data = json.loads(self.formdata)
+        response = self.client.post('/restaurant/book', json_data, content_type='application/json')  # views.book
+        self.assertEqual(response.status_code, 401)
+    
+    def test_authorization(self):
+        self.client.force_login(user=self.user)
+        response = self.client.get('/restaurant/bookings') # views.reservations
+        self.client.logout()
+        self.assertEqual(response.status_code, 200)
+
+    def test_200(self):
+        self.client.force_login(user=self.user)
+        json_data = json.loads(self.formdata)
+        response = self.client.post('/restaurant/book', json_data, content_type='application/json')  # views.book
+        self.client.logout()
+        self.assertEqual(response.status_code, 200)
+
+    def test_406(self):
+        self.client.force_login(user=self.user)
+        response = self.client.post('/restaurant/book', "test", content_type='application/json')  # views.book
+        self.client.logout()
+        self.assertEqual(response.status_code, 406)
+
