f86675792777af0a5a4dfaf34e0d541599790d08
fredtoh <fredtoh@gmail.com>
2023-02-07 11:54:30 -0800

    Updated app's url.py to include endpoint for bookings.

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('menu', views.MenuItemView.as_view(), name='menu'),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view(), name='menuitem'),
-    path('api-token-auth/', obtain_auth_token)
+    path('api-token-auth', obtain_auth_token),
+    path('book', views.BookingViewSet.as_view({'post':'create'}), name='book'),
+    path('bookings', views.BookingViewSet.as_view({'get': 'list'}), name='reservations'),
 ]
 
